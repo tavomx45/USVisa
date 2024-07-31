456ef8b361e38d3e751d74fedab7c30c3bbbb198
Gustavo Paredes <bryangustavoparedes@gmail.com>
2024-07-30 21:38:58 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa_approval",
+    version="0.1",
+    author="Gustavo Paredes",
+    author_email="bryangustavoparedes@gmail.com",
+    packages=find_packages(),
+)
